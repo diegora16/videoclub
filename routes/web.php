103b5e83f932a99055ef59dc7190c658c04124c5
diegora16 <diegoeureka982@gmail.com>
2024-05-24 22:33:16 -0500

    repositorio nuevo

---
+++ routes/web.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+//
+Route::get('/', function () {
+    return view('home');
+});
+
+//
+Route::get('login', function () {
+    return view('auth.login');
+});
+
+//
+Route::get('logout', function () {
+    return 'Logout usuario';
+});
+
+//
+Route::get('catalog', function () {
+    return view('catalog.index');
+});
+
+//
+Route::get('catalog/show/{id}', function ($id) {
+    return view('catalog.show');
+});
+
+//
+Route::get('catalog/create', function () {
+    return view('catalog.create');
+});
+
+//
+Route::get('catalog/edit/{id}', function ($id) {
+    return view('catalog.edit');
+});
